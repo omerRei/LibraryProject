456352013583df553fb0582b680e30e383de4b38
Omerrei <omerrei@post.bgu.ac.il>
2023-03-22 00:07:09 +0200

    second commit

--- src/main/java/Repository/BookRepository.java
+++ src/main/java/Repository/BookRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
     // Additional custom query methods can be added here if needed
+    public int t();
 }
